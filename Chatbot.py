2f07fe5b388fed0aadfa8370f1d62c7c6bc61aff
Nagavinay <155670397+satyalanagavinay@users.noreply.github.com>
2024-12-01 00:02:15 +0530

    Chatbot.py

---
+++ Chatbot.py
@@ -0,0 +1,16 @@
+import google.generativeai as ai
+
+API_KEY = '*************************'
+
+ai.configure(api_key=API_KEY)
+
+model = ai.GenerativeModel("gemini-pro")
+chat = model.start_chat()
+
+while True:
+    message = input('You: ')
+    if message.lower() == 'bye':
+        print('Chatbot: Goodbye!')
+        break
+    response = chat.send_message(message)
+    print('Chatbot:', response.text) 
